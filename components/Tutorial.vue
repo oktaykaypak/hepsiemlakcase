54074222cea6de24c1b47eac70eb58b12c83ddcc
oktaykaypak <36843265+oktaykaypak@users.noreply.github.com>
2021-08-02 00:21:15 +0300

    İlk Component
    Ürünlerin listeleneceği alanda kullanılacak olan itemlerin
    Componenti oluşturuldu

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,37 +1,17 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="column_50 item p_10">
-        <div class="row">
-          <div class="column_full display_flex content_center">
-            <img src="../assets/image/a.jpeg" alt="" class="column_75 " />
-          </div>
-        </div>
-        <div class="row">
-          <div class="column_full text_center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ullam.
-          </div>
-        </div>
-        <div class="row m_t_10">
-          <div
-            class="
-              column_25
-              text_bold text_large
-              display_flex
-              align_content_center
-            "
-          >
-            15₺
-          </div>
-          <div class="column_75 ">
-            <button class="btn btn_primary text_large text_mid ">Add Cart </button>
-          </div>
-        </div>
-      </div>
+      <item :product="product" />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      product: { name: 'deneme', price: 15 }
+    }
+  }
+}
 </script>
---
+++ components/item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="column_50 item p_10">
+    <div class="row">
+      <div class="column_full display_flex content_center">
+        <img src="../assets/image/a.jpeg" alt="image" class="column_75" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="column_full text_center">
+        {{ product.name }}
+      </div>
+    </div>
+    <div class="row m_t_10">
+      <div
+        class="column_25 text_bold text_large display_flex align_content_center"
+      >
+        {{ product.price }}₺
+      </div>
+      <div class="column_75">
+        <NuxtLink to="/about">
+          <button class="btn btn_primary text_large text_mid">Add Cart</button>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['product']
+}
+</script>
